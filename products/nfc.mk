74aa2552b72cb43ddb85b415bfddc31385c9b65d
Mark Dietz <imnuts@gmail.com>
2012-08-29 04:09:05 -0400

    The nfc.mk file

---
+++ products/nfc.mk
@@ -0,0 +1,5 @@
+# Stuff specific to NFC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/proprietary/app/TagGoogle.apk:system/app/Tag.apk \
+    $(LOCAL_PATH)/proprietary/app/Wallet.apk:system/app/Wallet.apk
+
